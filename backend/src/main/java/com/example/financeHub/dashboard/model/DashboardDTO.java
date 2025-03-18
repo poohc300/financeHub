6a9d07587754edbf4ba5ffd89c45f31ccd729a49
jeremy <jmkim12@i-gns.co.kr>
2025-03-18 15:39:01 +0900

    프론트이름 통일

---
+++ backend/src/main/java/com/example/financeHub/dashboard/model/DashboardDTO.java
@@ -0,0 +1,14 @@
+package com.example.financeHub.dashboard.model;
+import java.util.List;
+
+import com.example.financeHub.crawler.model.CrawledIpoDTO;
+import com.example.financeHub.crawler.model.CrawledNewsDTO;
+
+import lombok.Data;
+
+@Data
+public class DashboardDTO {
+
+    private List<CrawledNewsDTO> crawledNewsList;
+    private List<CrawledIpoDTO> crawledIpoList;
+}
