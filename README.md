d3e94033ffab2bc8b7a4563eda413760025f5d61
jeremy <poohc300@gmail.com>
2025-03-18 15:39:51 +0900

    오늘의 주요뉴스, 공모주 일정 출력 구현

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# financeInformation
+# financeHub
--- app/src/model/DashboardDataDTO.ts
+++ app/src/model/DashboardDataDTO.ts
@@ -14,21 +14,26 @@ export interface TopGainersDTO {
     volume: string; // 거래량량
 }
 
-export interface MarketEventsDTO {
+export interface CrawledNewsDTO {
     title: string; // 제목
     link: string; // 링크
 }
 
-export interface EconomicEventsDTO {
-    title: string; // 제목
+export interface CrawledIpoDTO {
+    companyName: string; // 종목명
     link: string; // 링크
+    period: string; // 공모주 일정
+    fixedOfferingPrice: string; // 확정공모가
+    expectedOfferingPrice: string; // 희망공모가
+    subscriptionRate: string; // 청약경쟁률
+    underWriter: string; // 주간사
 }
 
 export interface DashboardDataDTO {
     economicIndicators: EconomicIndicatorsDTO[];
     topGainers: TopGainersDTO[];
-    marketEvents: MarketEventsDTO[];
-    economicEvents: EconomicEventsDTO[];
+    crawledNewsList: CrawledNewsDTO[];
+    crawledIpoList: CrawledIpoDTO[];
 }
 
 
